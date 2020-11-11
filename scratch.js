2ea1e7a53b71b40c50d59c9c83dc63c47fb4385d
MARIKO ALVARADO <marikoaalvarado@gmail.com>
2020-11-10 22:31:23 -0800

    lab 07

---
+++ scratch.js
@@ -0,0 +1,14 @@
+// function handleWeather(request, response) {
+//   try {
+//     let weatherData = require('./data/weather.json');
+//     let weatherArr = weatherData.map(value => (new Forecast(value)))
+//     console.log(weatherArr)
+//     // let dateWeather = [];
+//     // weatherData.data.forEach((value) => {
+//     //   dateWeather.push(new Forecast(value))
+//     //   console.log(dateWeather);
+//   }
+//     response.send(weatherArr);
+// } catch (error) {
+//   console.error(error);
+// }
