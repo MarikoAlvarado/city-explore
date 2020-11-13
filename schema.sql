1daf4f7846b7b6da5e4f3675278b0f09891ed56c
MARIKO ALVARADO <marikoaalvarado@gmail.com>
2020-11-12 21:03:01 -0800

    trails,weather,location

--- schema.sql
+++ schema.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS location;
 CREATE TABLE location
 (
   id SERIAL PRIMARY KEY,
-  city VARCHAR(255),
-  city_info VARCHAR(255),
-  lat VARCHAR(255),
-  lon VARCHAR(255)
+  search_query VARCHAR(255),
+  formatted_query VARCHAR(255),
+  latitude VARCHAR(255),
+  longitude VARCHAR(255)
 );
